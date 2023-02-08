870b8cebc80ebaa0fa0a732c81c313a9797c3d39
chenxiangan <1598828268@qq.com>
2023-02-08 14:44:15 +0800

    服务的运用

---
+++ DownloadServerDemo/app/src/main/java/com/example/wx_asynctask/DownloadListener.java
@@ -0,0 +1,15 @@
+package com.example.wx_asynctask;
+
+public interface DownloadListener {
+    //通知当前下载进度
+    void onProgress(int progress);
+    //通知下载成功事件
+    void onSuccess();
+    // 通知下载失败事件
+    void onFailed();
+    // 通知下载暂停事件
+    void onPaused();
+    // 通知下载取消事件
+    void onCanceled();
+
+}
---
+++ DownloadServerDemo/app/src/main/java/com/example/wx_asynctask/DownloadTask.java
@@ -0,0 +1,170 @@
+package com.example.wx_asynctask;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+//代码使用模版，启动方式new DownloadTask().execute()
+//参数1：传入的参数给后台，参数2：使用整型数据作为进度单位，参数3：使用整型数据来反馈结果。
+
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+    private boolean isCanceled = false;
+    private boolean isPaused = false;
+    private int lastProgress;
+
+    public DownloadTask(DownloadListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected Integer doInBackground(String... params) {
+        //这个方法中的所有代码都在子线程中运行，所有耗时操作都在这里做。
+        InputStream is = null;
+        RandomAccessFile savedFile = null;
+        File file = null;
+        try {
+            long downloadedLength = 0;//记录已下载文件的长度
+            String downloadUrl = params[0];
+            String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+            //SD卡的download目录
+            String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+            file = new File(directory + fileName);
+            if (file.exists()) {
+                downloadedLength = file.length();
+            }
+            long contentLength = getContentLength(downloadUrl);
+            if (contentLength == 0) {
+                return TYPE_FAILED;
+            } else if (contentLength == downloadedLength) {
+                return TYPE_SUCCESS;
+            }
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .addHeader("RANGE", "bytes=" + downloadedLength + "-")
+                    .url(downloadUrl)
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response != null) {
+                is = response.body().byteStream();
+                savedFile = new RandomAccessFile(file, "rw");
+                savedFile.seek(downloadedLength);
+                byte[] b = new byte[1024];
+                int total = 0;
+                int len;
+                while ((len = is.read(b)) != -1) {
+                    //判断用户有没有点击取消按钮之类的。
+                    if (isCanceled) {
+                        return TYPE_CANCELED;
+                    } else if (isPaused) {
+                        return TYPE_PAUSED;
+                    } else {
+                        total += len;
+                        savedFile.write(b, 0, len);
+                        //计算下载的百分比
+                        int process = (int) ((total + downloadedLength) * 100 / contentLength);
+                        publishProgress(process);
+                        Thread.sleep(100);
+                    }
+                }
+                response.body().close();
+                return TYPE_SUCCESS;
+            }
+            //断点下载，指定从哪个字节开始
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+                if (savedFile != null) {
+                    savedFile.close();
+                }
+                if (isCanceled && file != null) {
+                    file.delete();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return TYPE_FAILED;
+        }
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        //UI更新操作
+        //在这里更新下载进度,该方法的携带的参数是从后台任务传递过来的。在这个方法中可以进行ui操作。
+        int progress = values[0];
+        //和上次进度对比，如果有变化的话则调用onProgress
+        if (progress > lastProgress) {
+            listener.onProgress(progress);
+            lastProgress = progress;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Integer status) {
+        //收尾工作
+        //后台任务执行完成，并通过return进行返回时。该方法调用。
+//        progressDialog.dismiss();//关闭进度对话框
+//        //在这里显示下载结果
+//        if (result) {
+//            Toast.makeText(context, "Download succeded", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(context, "Download failed", Toast.LENGTH_SHORT).show();
+//        }
+        switch (status) {
+            case TYPE_SUCCESS:
+                listener.onSuccess();
+                break;
+            case TYPE_FAILED:
+                listener.onFailed();
+                break;
+            case TYPE_PAUSED:
+                listener.onPaused();
+            case TYPE_CANCELED:
+                listener.onCanceled();
+            default:
+                break;
+        }
+    }
+
+    public void pauseDownload() {
+        isPaused = true;
+    }
+
+    public void cancelDownload() {
+        isCanceled = true;
+    }
+
+    private long getContentLength(String downloadUrl) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(downloadUrl)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response != null && response.isSuccessful()) {
+            long contentLength = response.body().contentLength();
+            response.close();
+            return contentLength;
+        }
+        return 0;
+    }
+}
+
